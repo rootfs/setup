56fc1443427d4b438a9afa858fc63a2f1b473e8b
rootfs <hchen@redhat.com>
2014-11-06 14:54:01 -0500

    tested on devstack
    Signed-off-by: rootfs <hchen@redhat.com>

--- openstack/devstack-install.sh
+++ openstack/devstack-install.sh
@@ -12,7 +12,7 @@ git clone git://github.com/openstack-dev/devstack.git
 cd devstack
 cat >> local.conf <<EOF
 [[local|localrc]]
-FLOATING_RANGE=192.168.1.0/24
+FLOATING_RANGE=192.168.1.224/27
 FIXED_RANGE=10.11.12.0/24
 FIXED_NETWORK_SIZE=256
 FLAT_INTERFACE=eth0
